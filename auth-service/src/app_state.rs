cd4210553ac3110fda5de3639c4a3c1ea008c14c
Ben Kimball <40365+benkimball@users.noreply.github.com>
2026-02-24 13:26:23 -0800

    Include banned token store in app state

--- auth-service/src/app_state.rs
+++ auth-service/src/app_state.rs
@@ -1,29 +1,40 @@
 use tokio::sync::RwLock;
 
-use crate::{domain::UserStore, services::HashMapUserStore};
+use crate::{
+    domain::{BannedTokenStore, UserStore},
+    services::{HashMapUserStore, HashSetBannedTokenStore},
+};
 use std::sync::Arc;
 
+// The Arc is necessary because trait objects (e.g., dyn UserStore) are
+// dynamically-sized types (DSTs) whose size is not known at compile time.
 pub type UserStoreType = Arc<RwLock<dyn UserStore + Send + Sync>>;
+pub type BannedTokenStoreType = Arc<RwLock<dyn BannedTokenStore + Send + Sync>>;
 
 /// Axum application state
 /// Only includes a user store for now, will likely include more state in the future.
 #[derive(Clone)]
 pub struct AppState {
-    // The Arc is necessary because trait objects (dyn UserStore) are
-    // dynamically-sized types (DSTs) whose size is not known at compile time.
     pub user_store: UserStoreType,
+    pub banned_token_store: BannedTokenStoreType,
 }
 
 impl AppState {
-    pub fn new(user_store: UserStoreType) -> Self {
-        Self { user_store }
+    pub fn new(user_store: UserStoreType, banned_token_store: BannedTokenStoreType) -> Self {
+        Self {
+            user_store,
+            banned_token_store,
+        }
     }
 }
 
 /// Create a new AppState from a concrete user store.
 impl<T: UserStore + 'static> From<T> for AppState {
     fn from(store: T) -> Self {
-        Self::new(Arc::new(RwLock::new(store)))
+        Self::new(
+            Arc::new(RwLock::new(store)),
+            Arc::new(RwLock::new(HashSetBannedTokenStore::default())),
+        )
     }
 }
 
@@ -32,6 +43,7 @@ impl Default for AppState {
     fn default() -> Self {
         Self {
             user_store: Arc::new(RwLock::new(HashMapUserStore::default())),
+            banned_token_store: Arc::new(RwLock::new(HashSetBannedTokenStore::default())),
         }
     }
 }
