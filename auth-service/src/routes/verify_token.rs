fa164a47ecdce1cfcd80fc67ac439d9ec5a02c66
Ben Kimball <40365+benkimball@users.noreply.github.com>
2026-02-24 15:17:54 -0800

    S3T5: Ban tokens on logout

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,14 +1,25 @@
 use crate::{
+    app_state::AppState,
     domain::{AuthApiError, Token},
     utils::auth::validate_token,
 };
-use axum::Json;
+use axum::{extract::State, Json};
 use reqwest::StatusCode;
 use serde::Deserialize;
 
 pub async fn verify_token(
+    State(state): State<AppState>,
     Json(request): Json<VerifyTokenRequest>,
 ) -> Result<StatusCode, AuthApiError> {
+    if state
+        .banned_token_store
+        .read()
+        .await
+        .is_banned(&request.token)
+        .await
+    {
+        return Err(AuthApiError::InvalidToken);
+    }
     validate_token(&request.token).await?;
     Ok(StatusCode::OK)
 }
--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -24,6 +24,23 @@ async fn should_return_401_if_invalid_token() {
 }
 
 #[tokio::test]
+async fn should_return_401_if_token_is_banned() {
+    let app = TestApp::new().await;
+    let email = get_random_email().parse().expect("Failed to parse email");
+    let token = generate_auth_token(&email).expect("Failed to generate auth token");
+    app.banned_token_store
+        .write()
+        .await
+        .ban(token.clone())
+        .await;
+    let body = json!({
+        "token": token
+    });
+    let response = app.post_verify_token(&body).await;
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[tokio::test]
 async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
     let body = json!({
